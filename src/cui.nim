4ca8b38de7770aca9f06803f75d99b045a1b5206
Bryan Patrick Coleman <totalknowledge@gmail.com>
2019-03-04 21:57:55 -0500

    Changed to Runes, and added different styles.

--- src/cui.nim
+++ src/cui.nim
@@ -1,14 +1,19 @@
 # Simple Text UI (TUI) for creating console applications that have
 # a simple event loop.
-import terminal, tables
+import terminal, tables, unicode, threadpool
+
+type runeStyle = tuple
+  bgStyle: BackgroundColor
+  fgStyle: ForegroundColor
+  txtStyles: set[Style]
 
 type Console* = object
   ## A coroutine state.
   width*: int      #Number of Columns
   height*: int     #Number of Rows
-  buffer: seq[string]
+  buffer: seq[seq[Rune]]
   executeQueue: Table[char, seq[proc()]]
-  styleQueue: seq[seq[BackgroundColor]]
+  styleQueue: seq[seq[runeStyle]]
 
 proc init*(self: var Console) =
   stdout.hideCursor()
@@ -18,11 +23,11 @@ proc init*(self: var Console) =
   var ith = self.height
   while(ith > 0):
     var itw = self.width
-    var row = ""
+    var row: seq[Rune]
     self.styleQueue.add(@[])
     while(itw > 0):
-      self.styleQueue[self.height-ith].add(bgDefault)
-      row = $row & " "
+      self.styleQueue[self.height-ith].add((bgStyle: bgDefault, fgStyle: fgDefault, txtStyles: {}))
+      row.add(" ".toRunes)
       itw.dec()
     self.buffer.add(row)
     ith.dec()
@@ -31,49 +36,63 @@ proc write*(self: Console) =
   stdout.setCursorPos(0,0)
   for i, line in pairs(self.buffer):
     var temp_seq = self.styleQueue[i]
-    for i, wchar in line:
-      stdout.setBackgroundColor(temp_seq[i])
-      stdout.write(wchar)
+    for i, wRune in pairs(line):
+      stdout.setBackgroundColor(temp_seq[i].bgStyle)
+      stdout.setForegroundColor(temp_seq[i].fgStyle)
+      stdout.setStyle(temp_seq[i].txtStyles)
+      stdout.write(wRune)
+    stdout.resetAttributes()
 
 proc place*(self: var Console, str: string, row: int, col: int) =
-  self.buffer[row][col..col+(str.len-1)] = str
+  var inc = 0
+  for r in runes(str):
+    self.buffer[row][col+inc] = r
+    inc.inc()
 
 proc place*(self: var Console, str: string, row: int, pos: string) =
   var low_col = 0
-  var high_col = 0
   var cal_pos = 0
   case pos:
     of "center":
       cal_pos = int(self.width/2)
       low_col = cal_pos - int(str.len/2)
-      high_col = low_col + (str.len-1)
     of "right":
       low_col = self.width - (str.len)
-      high_col = self.width - 1
-    else:
-      high_col = str.len - 1
-  self.buffer[row][low_col..high_col] = str
+  var inc = 0
+  for r in runes(str):
+    self.buffer[row][low_col+inc] = r
+    inc.inc()
 
-proc setRowStyle*(self: var Console, row: int, style: BackgroundColor) =
+proc setRowStyle*(self: var Console,
+                  row: int,
+                  bgstyle: BackgroundColor = bgDefault,
+                  fgstyle: ForegroundColor = fgDefault,
+                  txtstyles: set[Style] = {}) =
   for key, value in pairs(self.styleQueue[row]):
-    self.styleQueue[row][key] = style
+    self.styleQueue[row][key].bgStyle = bgstyle
+    self.styleQueue[row][key].fgStyle = fgstyle
+    self.styleQueue[row][key].txtStyles = txtstyles
 
 proc run*(self: var Console) =
   self.write()
   var bob: char
   while(int(bob) != 3):
-    bob = getch()
+    let boblet = spawn getch()
+    bob = ^boblet
+    self.place($bob, 5, 5)
     self.write()
 
 ## Main create test
-enableTrueColors()
-var s: Console
-s.init()
-s.place("Hello World", 0, "center")
-s.setRowStyle(0, bgRed)
-s.run()
+when isMainModule:
+  enableTrueColors()
+  var s: Console
+  s.init()
+  s.place("Hello \u2503", 0, "center") #"\u5f7c"
+  s.place("MIT Boo 0.1.1", 0, 3)
+  for i in 1..5:
+    s.place("\u258F", i, 0)
+  s.setRowStyle(0, bgRed)
+  s.run()
 
-stdout.showCursor()
-stdout.resetAttributes()
-
-#echo isTrueColorSupported()
+  stdout.showCursor()
+  stdout.resetAttributes()
